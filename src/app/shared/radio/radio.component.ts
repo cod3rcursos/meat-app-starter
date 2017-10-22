e4151e9108e5a3417402e745788cc1cee73ae769
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-10-22 20:06:55 -0200

    PaymentOption para Order Component

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
 import { RadioOption } from './radio-option.model';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'mt-radio',
   templateUrl: './radio.component.html',
-  styleUrls: ['./radio.component.css']
+  styleUrls: ['./radio.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
   @Input() options: RadioOption[];
   value: any;
+  onChange: any;
 
   constructor() {}
 
@@ -16,5 +25,16 @@ export class RadioComponent implements OnInit {
 
   setValue(value: any) {
     this.value = value;
+    this.onChange(this.value);
   }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched() {}
 }
