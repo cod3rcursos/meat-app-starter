d50a37ca9906908668834dbb31d92477c638bf25
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-10-22 02:48:08 -0200

    Prettier fix

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
