23e345e3db3c1454b2b18cfeb974536882b973ed
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-10-22 01:45:27 -0200

    Criação de primeiras rotas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+export const ROUTES: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent}
+];
