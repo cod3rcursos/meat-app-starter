94d3156107b511692a3bc9cb06e7a0b18017014f
Carol <cvsouza04@gmail.com>
2019-03-04 16:29:56 -0300

    Criação do componente Restaurantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'restaurants', component: RestaurantsComponent },
   { path: 'about', component: AboutComponent }
 ]
---
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -0,0 +1,10 @@
+export interface Restaurant {
+  id: string,
+  name: string,
+  category: string,
+  deliveryEstimate: string,
+  rating: number,
+  imagePath: string,
+  about: string,
+  hours: string
+}
