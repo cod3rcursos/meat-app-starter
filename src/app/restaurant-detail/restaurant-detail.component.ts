e6af4cfe9e9c505156d76f80ce84f46de8c9fa4c
Carol <cvsouza04@gmail.com>
2019-03-12 00:04:57 -0300

    Detalhes do restaurante vindo da API fake

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { RestaurantsService } from '../restaurants/restaurants.service';
+
+import { Restaurant } from '../restaurants/restaurant/restaurant.model';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,9 +11,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant;
+
+  constructor(private restaurantsService: RestaurantsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant);
   }
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -20,4 +20,10 @@ export class RestaurantsService {
       .catch(ErrorHandler.handleError);
   }
 
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
 }
