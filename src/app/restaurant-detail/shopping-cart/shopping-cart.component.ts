8c0c19a21fdae5844c557e957272a2b9716f66a2
Carol <cvsouza04@gmail.com>
2019-03-25 21:44:40 -0300

    Finalização do Carrinho de Compras

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -16,7 +16,19 @@ export class ShoppingCartComponent implements OnInit {
     return this.shoppingCartService.items;
   }
 
-  total(): number { 
+  clear() {
+    this.shoppingCartService.clear();
+  }
+
+  addItem(item: any) {
+    this.shoppingCartService.addItem(item);
+  }
+
+  removeItem(item: any) {
+    this.shoppingCartService.removeItem(item);
+  }
+
+  total(): number {
     return this.shoppingCartService.total();
   }
 
