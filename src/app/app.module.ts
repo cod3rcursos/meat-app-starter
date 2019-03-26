cc293cd052b0f07ad1ba4f9384a36bede3c7df09
Carol <cvsouza04@gmail.com>
2019-03-25 21:52:38 -0300

    Formatação dos preços para pt-BR e finalização da versão 1.0 do sistema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -41,6 +41,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
   providers: [
     RestaurantsService,
     ShoppingCartService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [AppComponent]
 })
