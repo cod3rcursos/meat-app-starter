7de2ebb8825317889bdaa4b9e78f3167df8a4d34
Carol <cvsouza04@gmail.com>
2019-04-03 22:46:21 -0300

    Adaptando o serviço para receber dados das diretivas (ngModel)

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
 import { RadioOption } from './radio-option.model';
 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true,
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   @Input() options: RadioOption[];
 
   value: any;
+
+  onChange: any;
 
   constructor() { }
 
@@ -18,6 +29,19 @@ export class RadioComponent implements OnInit {
 
   setValue(value: any) {
     this.value = value;
+    this.onChange(this.value);
   }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void { } // Quando o usuário entra no componente
+
+  setDisabledState(isDisabled: boolean): void { }
 
 }
