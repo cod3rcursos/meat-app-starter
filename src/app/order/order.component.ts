8556d56cbe91455f0f3e5978f41235eb43029a55
Carol <cvsouza04@gmail.com>
2019-04-04 22:02:35 -0300

    POST do pedido para o back-end (json-server)
    Mockado no arquivo db.json

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
 
 @Component({
   selector: 'mt-order',
@@ -41,6 +42,15 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem) {
     this.orderService.remove(item);
+  }
+  checkOrder(order: Order) {
+    order.orderItems = this.cartItems()
+      .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order)
+      .subscribe((orderId: string) => {
+        console.log(`Compra concluída: ${orderId}`);
+        this.orderService.clear();
+      });
   }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from "@angular/core";
+
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
+import { Order, OrderItem } from './order.model';
+
+import { MEAT_API } from '../app.api';
 
 @Injectable()
 export class OrderService {
-  constructor(private cartService: ShoppingCartService) { }
+  constructor(private cartService: ShoppingCartService, private http: Http) { }
 
   itemsValue(): number {
     return this.cartService.total();
@@ -24,6 +32,17 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.cartService.removeItem(item);
+  }
+
+  clear() {
+    this.cartService.clear();
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers;
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({ headers: headers }))
+      .map(response => response.json());
   }
 
 }
