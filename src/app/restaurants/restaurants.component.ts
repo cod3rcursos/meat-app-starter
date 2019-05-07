cc59e331d9bcfaa44333e9239a1fd02ce5b883b5
Carol Vasconcelos <carol.vasconcelos080@gmail.com>
2019-05-07 14:55:46 -0300

    Finalização da busca dos restaurantes

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -6,6 +6,8 @@ import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantsService } from './restaurants.service';
 
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'mt-restaurants',
@@ -43,6 +45,8 @@ export class RestaurantsComponent implements OnInit {
     });
 
     this.searchControl.valueChanges
+      .debounceTime(500)
+      .distinctUntilChanged()
       .switchMap(searchTerm => this.restaurantsService.restaurants(searchTerm))
       .subscribe(restaurants => this.restaurants = restaurants);
 
