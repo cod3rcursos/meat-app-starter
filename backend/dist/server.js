e3e06bd7f4963c205b3af8c0c32bbc840329ae6a
Carol <cvsouza04@gmail.com>
2019-05-21 20:50:37 -0300

    Primeiras configurações do backend

---
+++ backend/dist/server.js
@@ -0,0 +1,26 @@
+"use strict";
+exports.__esModule = true;
+var jsonServer = require("json-server");
+var fs = require("fs");
+var https = require("https");
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+// Add custom routes before JSON Server router
+server.get('/echo', function (req, res) {
+    res.jsonp(req.query);
+});
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+// Use default router
+server.use(router);
+var options = {
+    cert: fs.readFileSync('./backend/keys/cert.pem'),
+    key: fs.readFileSync('./backend/keys/key.pem')
+};
+https.createServer(options, server).listen(3001, function () {
+    console.log('JSON Server is running on https://localhost:3001');
+});
