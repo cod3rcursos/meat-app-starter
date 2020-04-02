92aaa37f786db434e6f45a1f250ed264306920d8
Carol Vasconcelos <carol.vasconcelos080@gmail.com>
2020-04-02 19:59:01 -0300

    WIP: Ajustes deploy no heroku (4)

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,6 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-const path = require('path');
-// const configPath = path.join(__dirname, '../dist');
-
-// const app = express();
-
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
 
@@ -16,18 +11,12 @@ server.use(middlewares)
 server.get('/echo', (req, res) => {
   res.jsonp(req.query)
 })
-
-// server.use(app.static(configPath));
-
-server.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
-})
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 
 // Use default router
 server.use(router)
-server.listen(process.env.PORT || 3001, () => {
+server.listen(3001, () => {
   console.log('JSON Server is running')
 })
