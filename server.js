db06b73ed3a5dd7e89c2f57b2d748c3fafc4ccd9
Carol Vasconcelos <carol.vasconcelos080@gmail.com>
2020-04-02 20:08:16 -0300

    WIP: Ajustes deploy no heroku (6)

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '../dist/'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'../dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
