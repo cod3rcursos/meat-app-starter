e40d9fade59d6722e7145bd900034dd5830cbbba
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-05-04 12:42:55 -0300

    Criação do componente about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
