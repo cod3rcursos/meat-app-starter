689ff44f10a2e0e114f9ee1a688541873ce6269e
Ventura <90869975+ventur09@users.noreply.github.com>
2021-12-27 20:47:29 -0500

    Create restaurants.component.ts

---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html'
+})
+export class RestaurantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
