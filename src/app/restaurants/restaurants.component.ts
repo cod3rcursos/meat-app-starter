f1b992e4861ba78760ba5745f607899dc8ddd4ce
Ventura <90869975+ventur09@users.noreply.github.com>
2021-12-27 21:27:36 -0500

    Update restaurants.component.ts

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Restaurant } from "./restaurant/restaurant.model";
 
 @Component({
-  selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  selector: "mt-restaurants",
+  templateUrl: "./restaurants.component.html",
 })
 export class RestaurantsComponent implements OnInit {
+  restaurants: Restaurant[] = [
+    {
+      id: "bread-bakery",
+      name: "Bread & Bakery",
+      category: "Bakery",
+      deliveryEstimate: "25m",
+      rating: 4.9,
+      imagePath: "assets/img/restaurants/breadbakery.png",
+      about:
+        "A Bread & Brakery tem 40 anos de mercado. Fazemos os melhores doces e pães. Compre e confira.",
+      hours: "Funciona de segunda à sexta, de 8h às 23h",
+    },
+    {
+      id: "burger-house",
+      name: "Burger House",
+      category: "Hamburgers",
+      deliveryEstimate: "100m",
+      rating: 3.5,
+      imagePath: "assets/img/restaurants/burgerhouse.png",
+      about: "40 anos se especializando em trash food.",
+      hours: "Funciona todos os dias, de 10h às 22h",
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
