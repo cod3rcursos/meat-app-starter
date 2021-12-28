5c44f857f648b1a52217c821d2264d6f892a8fae
Ventura <90869975+ventur09@users.noreply.github.com>
2021-12-27 20:46:45 -0500

    Update app.module.ts
    add component restaurant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { ROUTES } from "./app.routes";
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent, RestaurantsComponent],
   imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent],
