ad898ad6f0034c9f7b773300e7fdc88a325d911c
Ventura <90869975+ventur09@users.noreply.github.com>
2021-12-27 20:18:48 -0500

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'mt-home',
-  templateUrl: './home.component.html'
+  selector: "mt-home",
+  templateUrl: "./home.component.html",
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
