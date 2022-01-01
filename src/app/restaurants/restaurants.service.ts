8cf1496a518ffcae3a9a2f739f629b009f1b3010
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-01 14:41:59 -0500

    add HTTP ao serviço de restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,32 +1,19 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+
+import { MEAT_API } from "app/app.api";
+import { Observable } from "rxjs";
+import "rxjs/add/operator/map";
+
 import { Restaurant } from "./restaurant/restaurant.model";
 
+@Injectable()
 export class RestaurantsService {
-  rests: Restaurant[] = [
-    {
-      id: "bread-bakery",
-      name: "Bread & Bakery",
-      category: "Bakery",
-      deliveryEstimate: "25m",
-      rating: 4.9,
-      imagePath: "assets/img/restaurants/breadbakery.png",
-      about:
-        "A Bread & Brakery tem 40 anos de mercado. Fazemos os melhores doces e pães. Compre e confira.",
-      hours: "Funciona de segunda à sexta, de 8h às 23h",
-    },
-    {
-      id: "burger-house",
-      name: "Burger House",
-      category: "Hamburgers",
-      deliveryEstimate: "100m",
-      rating: 3.5,
-      imagePath: "assets/img/restaurants/burgerhouse.png",
-      about: "40 anos se especializando em trash food.",
-      hours: "Funciona todos os dias, de 10h às 22h",
-    },
-  ];
-  constructor() {}
+  constructor(private http: Http) {}
 
-  restaurants(): Restaurant[] {
-    return this.rests;
+  restaurants(): Observable<Restaurant[]> {
+    return this.http
+      .get(`${MEAT_API}/restaurants`)
+      .map((response) => response.json());
   }
 }
