27b0d1f03e86ec37162f81ca31c9b90a1648b1b8
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-01 20:11:27 -0500

    Implementação do componente de avaliação  -pipes async e date

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'mt-reviews',
-  templateUrl: './reviews.component.html'
+  selector: "mt-reviews",
+  templateUrl: "./reviews.component.html",
 })
 export class ReviewsComponent implements OnInit {
+  reviews: Observable<any>;
 
-  constructor() { }
+  constructor(
+    private restaurantsService: RestaurantsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.reviews = this.restaurantsService.reviewsOfRestaurant(
+      this.route.parent.snapshot.params["id"]
+    );
   }
-
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -19,4 +19,18 @@ export class RestaurantsService {
       .map((response) => response.json())
       .catch(ErrorHandler.handleError);
   }
+
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}`)
+      .map((response) => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any> {
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map((response) => response.json())
+      .catch(ErrorHandler.handleError);
+  }
 }
