a81a8ce5ddf393e27de10c22daa678f61a43916f
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-01 19:46:28 -0500

    rotas filhas para avaliações e menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
+import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
 
 export const ROUTES: Routes = [
   { path: "", component: HomeComponent },
   { path: "restaurants", component: RestaurantsComponent },
-  { path: "restaurants/:id", component: RestaurantDetailComponent },
+  {
+    path: "restaurants/:id",
+    component: RestaurantDetailComponent,
+    children: [
+      { path: "", redirectTo: "menu", pathMatch: "full" },
+      { path: "menu", component: MenuComponent },
+      { path: "reviews", component: ReviewsComponent },
+    ],
+  },
   { path: "about", loadChildren: "./about/about.module#AboutModule" },
 ];
