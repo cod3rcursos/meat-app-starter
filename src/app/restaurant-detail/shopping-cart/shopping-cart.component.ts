0ad968a00286231f6555d563e82fdccca71b4dc0
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-01 19:28:22 -0500

    Create shopping-cart.component.ts

---
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-shopping-cart',
+  templateUrl: './shopping-cart.component.html'
+})
+export class ShoppingCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
