f74118e291d9b1ac63b26d980ffecf6de769dac8
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-01 19:28:38 -0500

    Create menu.component.ts

---
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-menu',
+  templateUrl: './menu.component.html'
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
