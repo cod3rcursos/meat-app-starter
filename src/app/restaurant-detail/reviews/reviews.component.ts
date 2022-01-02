f3f5d21a2118bbef8cc309ec33e67983e002d74e
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-01 19:38:31 -0500

    Create reviews.component.ts

---
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-reviews',
+  templateUrl: './reviews.component.html'
+})
+export class ReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
