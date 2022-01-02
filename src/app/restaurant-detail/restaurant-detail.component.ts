bcf288ed6a94b7058c2588999830fff491d40333
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-01 19:28:31 -0500

    Create restaurant-detail.component.ts

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html'
+})
+export class RestaurantDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
