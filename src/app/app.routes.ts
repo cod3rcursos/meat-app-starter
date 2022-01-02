773a6348e1397fe23905179fc7e86570e665211b
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-01 19:33:44 -0500

    add  rotas para o detalhes dos restaurantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
 
 export const ROUTES: Routes = [
   { path: "", component: HomeComponent },
   { path: "restaurants", component: RestaurantsComponent },
+  { path: "restaurants/:id", component: RestaurantDetailComponent },
   { path: "about", loadChildren: "./about/about.module#AboutModule" },
 ];
