cf997ac3f081239f755bd6f48266eb1c1716b7ed
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-23 19:16:17 -0300

    ControlValueAccessor

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, forwardRef, Input, OnInit } from "@angular/core";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 import { RadioOption } from "./radio-option.model";
 
 @Component({
   selector: "mt-radio",
   templateUrl: "./radio.component.html",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true,
+    },
+  ],
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
   @Input() options: RadioOption[];
 
   value: any;
+
+  onChange: any;
 
   constructor() {}
 
@@ -16,5 +26,19 @@ export class RadioComponent implements OnInit {
 
   setValue(value: any) {
     this.value = value;
+    this.onChange(this.value);
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error("Method not implemented.");
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error("Method not implemented.");
   }
 }
