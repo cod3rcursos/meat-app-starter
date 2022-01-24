0a87b7005e398f4462b330fe6bfee874a71ad116
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-23 22:36:25 -0300

    checkOrder

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { RadioOption } from "app/shared/radio/radio-option.model";
+import { Order, OrderItem } from "./order.model";
 import { OrderService } from "./order.service";
 
 @Component({
@@ -36,5 +37,17 @@ export class OrderComponent implements OnInit {
   }
   remove(item: CartItem) {
     this.orderService.remove(item);
+  }
+
+  checkOrder(order: Order) {
+    order.orderItems = this.cartItems().map(
+      (item: CartItem) => new OrderItem(item.quantity, item.menuItem.id)
+    );
+
+    this.orderService.checkOrder(order).subscribe((orderId: string) => {
+      console.log(`Compra concluída: ${orderId}`);
+      this.orderService.clear();
+    });
+    console.log(order);
   }
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,10 +1,19 @@
+import { MEAT_API } from "app/app.api";
+import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { ShoppingCartServices } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { Order } from "./order.model";
+
+import { HttpClient } from "@angular/common/http";
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class OrderService {
-  constructor(private cartService: ShoppingCartServices) {}
+  constructor(
+    private cartService: ShoppingCartServices,
+    private http: HttpClient
+  ) {}
 
   itemsValue(): number {
     return this.cartService.total();
@@ -24,5 +33,15 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.cartService.removeItem(item);
+  }
+
+  clear() {
+    this.cartService.clear();
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    return this.http
+      .post<Order>(`${MEAT_API}/orders`, order)
+      .map((order) => order.id);
   }
 }
