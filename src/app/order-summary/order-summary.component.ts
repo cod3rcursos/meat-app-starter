07baeaa86d27348b5e542e5a86ebba58d5836de6
Ventura <90869975+ventur09@users.noreply.github.com>
2022-01-23 22:40:02 -0300

    Create order-summary.component.ts

---
+++ src/app/order-summary/order-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-order-summary',
+  templateUrl: './order-summary.component.html',
+  styleUrls: ['./order-summary.component.css']
+})
+export class OrderSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
